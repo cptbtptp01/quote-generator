fb42b86cdf40e4e498784c63b783aa21e96b9452
Chiputao <420958094@qq.com>
2023-05-31 22:45:36 -0700

    updated front-end tasks.

--- components/quoteGenerator/quoteGenerator.tsx
+++ components/quoteGenerator/quoteGenerator.tsx
@@ -2,21 +2,132 @@ import styled from "styled-components";
 import Link from "next/link";
 import Image from "next/image";
 
-export const GradientBackgroundCon = styled.div`
-    background: linear-gradient(to right, #000046, #1cb5e0);
+export const WhiteBackgroundCon = styled.div`
+    background: white;
     background-size: 400% 400%;
-    animation: gradient 6s ease infinite;
+    animation: gradient 15s ease infinite;
     height: 100vh; /* view height */
     width: 100vw;
-    @keyframes gradient {
-    0% {
-        background-position: 0% 50%;
+    position: absolute;
+`;
+
+export const BackgroundImage1 = styled(Image)`
+    position: relative;
+    z-index: 1;
+    margin-left: -10px;
+    margin-top: -10px;
+`;
+
+export const BackgroundImage2 = styled(Image)`
+    position: fixed;
+    z-index: 1;
+    right: -90px;
+    bottom: -10px;
+`;
+
+export const FooterCon = styled.div`
+    width: 100vw;
+    height: 50px;
+    text-align: center;
+    font-family: 'Roboto Mono', monospace;
+    font-size: 14px;
+    position: absolute;
+    bottom: 0;
+    color: black;
+    z-index: 999999;
+`;
+
+export const FooterLink = styled(Link)`
+    color: black;
+`;
+
+export const QuoteGeneratorCon= styled.div`
+    min-height: 350px;
+    min-width: 350px;
+    height: 70vh;
+    width: 70vw;
+    border-radius: 0px;
+    //center
+    top: 50%; 
+    left: 50%;
+    transform: translate(-50%,-50%);
+    position: absolute;
+    // above background
+    z-index: 2; 
+`;
+
+export const QuoteGeneratorInnerCon = styled.div`
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    position: absolute;
+    width: 100%;
+`;
+
+export const QuoteGeneratorTitle = styled.div`
+    font-family: 'Roboto Mono', monospace;
+    font-size: 24px;
+    color: black;
+    text-align: center;
+    // padding for the text
+    // top right bottom left
+    padding: 0px 20px 0px 20px; 
+    // center at the top
+    position: relative;
+    // media query
+    @media only screen and (max-width: 600px) {
+        font-size: 20px;
     }
-    50% {
-        background-position: 100% 50%;
+`;
+
+export const QuoteGeneratorSubTitle = styled.div`
+    font-family: 'Roboto Mono', monospace;
+    font-size: 14px;
+    color: black;
+    text-align: center;
+    // padding for the text
+    // top right bottom left
+    padding: 0px 20px 0px 20px;
+    // center at the top
+    position: relative;
+    width: 100%;
+    // media query
+    @media only screen and (max-width: 600px) {
+        font-size: 12px;
     }
-    100% {
-        background-position: 0% 50%;
+`;
+
+export const QuoteGeneratorButton = styled.div`
+    height: 30px;
+    width: 120px;
+    border: 2px solid darkgrey;
+    border-radius: 3px;
+    
+    margin-top: 20px;
+    position: relative;
+    transition: 0.2s all ease-in-out;
+    cursor: pointer;
+    top: 20px;
+    margin: auto;
+    transform-origin: center;
+
+    &:hover {
+        filter: brightness(3);
+        transition: 0.2s all ease-in-out;
+        transform: scale(1.1);
+
+        transform-origin: center;
     }
-    }
+`;
+
+export const GenerateQuetoButtonText = styled.div`
+    color: black;
+    font-family: 'Roboto Mono', monospace;
+    font-size: 14px;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    position: absolute;
+    width: 100%;
+    text-align: center;
 `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import React, { useState } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import { GradientBackgroundCon } from '../../components/quoteGenerator/quoteGenerator'
+// components 
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GenerateQuetoButtonText, WhiteBackgroundCon, QuoteGeneratorButton, QuoteGeneratorCon, QuoteGeneratorInnerCon, QuoteGeneratorSubTitle, QuoteGeneratorTitle } from '../../components/quoteGenerator/quoteGenerator'
+
+// assets
+import cloud1 from 'assets/cloud-and-thunder.png'
+import cloud2 from 'assets/cloudy-weather.png'
 
 export default function Home() {
+  // HOME
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -12,10 +22,58 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       {/* background */}
-      <GradientBackgroundCon>
-        
-      </GradientBackgroundCon>
+      <WhiteBackgroundCon>
+
+        {/* quote generator modal pop-up */}
+        {/* <QuoteGeneratorModal 
+        /> */}
+
+        {/* quote generator */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+
+            <QuoteGeneratorTitle>
+              Your Daily Quote Generator
+            </QuoteGeneratorTitle>
+
+            <QuoteGeneratorSubTitle>
+              Click the button below to generate a random quote! 
+              provided by <FooterLink href="https://zenquotes.io/" target="_blank" rel="noopener noreferrer">ZenQuotes API</FooterLink>.
+            </QuoteGeneratorSubTitle>
+
+            <QuoteGeneratorButton>
+              <GenerateQuetoButtonText onClick={null}> Get Inspired!
+              </GenerateQuetoButtonText>
+            </QuoteGeneratorButton>
+
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        {/* Background Images */}
+        <BackgroundImage1
+          src={cloud1}
+          height="300"
+          alt="cloudybackground1"
+        />
+
+        <BackgroundImage2
+          src={cloud2}
+          height="250"
+          alt="cloudybackground2"
+        />
+
+        {/* footer container */}
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed by <FooterLink href="https://github.com/cptbtptp01" target="_blank" rel="noopener noreferrer"> @huiru </FooterLink>
+          </>
+        </FooterCon>
+
+      </WhiteBackgroundCon>
     </>
   )
 }
